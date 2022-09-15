83daffb7d17b7b6dcb2736af011809c844b8ab09
Admin <Admin@DESKTOP-96C0HGC>
2022-09-16 00:46:18 +0300

    Добавьте файлы проекта.

---
+++ YoutubeLinker/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace YoutubeLinker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generate = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(64, 119);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(75, 23);
+            this.Generate.TabIndex = 0;
+            this.Generate.Text = "GO";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(61, 54);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 248);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.Generate);
+            this.Name = "Form1";
+            this.Text = "Youtube Linker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ YoutubeLinker/Form1.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace YoutubeLinker
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            thread = new Thread(linkesStatus);
+            thread.Start();
+           
+        }      
+        private static Random random = new Random();
+
+        public static string GenerateLink(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+       public void linkesStatus()
+        {
+            var count = 50;
+            var API_KEY = "AIzaSyByyh9l7zc7_EBVgbR7LyrEYYTPp_SvpK0";
+            var q = GenerateLink(3);
+            var url = "https://www.googleapis.com/youtube/v3/search?key=" + API_KEY + "&maxResults=" + count + "&part=snippet&type=video&q=" + q;
+
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(url);
+                dynamic jsonObject = JsonConvert.DeserializeObject(json);
+                foreach (var line in jsonObject["items"])
+                {
+                    Action action = () => linkLabel1.Text = "https://www.youtube.com/watch?v=" + line["id"]["videoId"]; ;
+                    // Свойство InvokeRequired указывает, нeжно ли обращаться к контролу с помощью Invoke
+                    if (InvokeRequired)
+                        Invoke(action);
+                    else
+                        action(); 
+                    /*store your id*/
+                }
+
+            }
+        }
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Label label = sender as Label;
+
+            Process.Start(linkLabel1.Text);
+            if (label != null)
+            {
+                Clipboard.SetText(label.Text, TextDataFormat.UnicodeText);
+            }
+        }
+    }
+}
